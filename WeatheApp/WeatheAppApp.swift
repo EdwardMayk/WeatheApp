dd7cf7a9f8a2a3658809025a6878bb7afc943cd5
EdwardMayk <samaleminina322@gmail.com>
2023-07-29 00:32:15 -0500

    Initial Commit

---
+++ WeatheApp/WeatheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheAppApp.swift
+//  WeatheApp
+//
+//  Created by Edward Mayk on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
